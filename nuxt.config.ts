a39b80f5630f460d48970816ba1fdb13995eb5b3
theoxav <theoxxav@outlook.fr>
2025-03-24 16:43:15 +0100

    Add tailwindcss, nuxt-icon,loadash, stripe, pinia and supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  pages: true,
+  modules: [
+    '@nuxt/icon',
+    'nuxt-lodash',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    '@nuxtjs/tailwindcss',
+    // '@nuxtjs/supabase',
+  ],
+  runtimeConfig: {
+    public: {
+      stripePk: process.env.STRIPE_PK,
+    },
+  },
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://js.stripe.com/v3/',
+          defer: true,
+        },
+      ],
+    },
+  },
+});
