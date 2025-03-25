43da660a655bf58af4acb30c42ffe4f6c7f1e675
theoxav <theoxxav@outlook.fr>
2025-03-25 12:15:57 +0100

    Add Product item component with fake data

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,89 @@
 <template>
-  <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">Index Page</div>
+  <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+    <div
+      class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4"
+    >
+      <div v-if="products" v-for="product in products" :key="product.id">
+        <ProductItem :product="product" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProductItem from '~/components/products/ProductItem.vue';
+
+const products = [
+  {
+    id: 1,
+    title: 'Title 1',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/7/800/800',
+    price: 999,
+  },
+  {
+    id: 2,
+    title: 'Title 2',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/29/800/800',
+    price: 999,
+  },
+  {
+    id: 3,
+    title: 'Title 3',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/26/800/800',
+    price: 999,
+  },
+  {
+    id: 4,
+    title: 'Title 4',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/22/800/800',
+    price: 999,
+  },
+  {
+    id: 5,
+    title: 'Title 5',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/17/800/800',
+    price: 999,
+  },
+  {
+    id: 6,
+    title: 'Title 6',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/13/800/800',
+    price: 999,
+  },
+  {
+    id: 7,
+    title: 'Title 7',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/14/800/800',
+    price: 999,
+  },
+  {
+    id: 8,
+    title: 'Title 8',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/20/800/800',
+    price: 999,
+  },
+  {
+    id: 9,
+    title: 'Title 9',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/15/800/800',
+    price: 999,
+  },
+  {
+    id: 10,
+    title: 'Title 10',
+    description: 'This is a description',
+    url: 'https://picsum.photos/id/12/800/800',
+    price: 999,
+  },
+];
+</script>
 <style></style>
